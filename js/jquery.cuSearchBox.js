22caf19ece1671f9f1631fdd0861caa84f943429
Matt Congel <conge101@mail.chapman.edu>
2015-08-17 16:32:07 -0700

    refactor queries (multi search possible)

--- js/jquery.cuSearchBox.js
+++ js/jquery.cuSearchBox.js
@@ -22,6 +22,86 @@
     </a>';
   // End of the lines must be escaped for correct javsacript syntax. 
 
+  /************************************/
+  /***** ::: STATIC FUNCTIONS ::: *****/
+  /************************************/
+
+  var resultHandlers = {};
+
+  // Result Type of "WWW"
+  resultHandlers.www = {
+
+    buildQuery: function(search_term) {
+      var head  = {index: 'chapcrawl'};
+      var body  = {query : {bool : {should : [{match: {meta_title: {query : search_term, boost : 7}}},{match: {page_title: {query : search_term, boost : 5}}},{match: {description: {query : search_term, boost : 3}}},{match: {_all: search_term}}]}}, size: 5, from: 0};
+      return JSON.stringify(head) + '\n' + JSON.stringify(body) + '\n';
+    },
+
+    buildResult: function(item) {
+      return buildHTML(filterItem({
+        title :       item.page_title,
+        image :       item.page_image,
+        description : $(item.description).text(),
+        url :         item.url
+      }));
+    }
+
+  };
+
+  // Result Type of "Event"
+  resultHandlers.event = {
+    buildQuery: function(search_term) {
+      var head = {index: 'events_development'};
+      var body = {query: {dis_max: {queries: [{match: {"title.word_start": {query: search_term, operator: "and", boost: 7, analyzer: "searchkick_word_search"}}}, {match: {"title.word_start": {query: search_term, operator: "and", boost: 5, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search"}}}, {match: {"title.word_start": {query: search_term, operator: "and", boost: 5, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search2"}}}, {match: {"_all": {query: search_term, operator: "and", boost: 3, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search"}}}, {match: {"_all": {query: search_term, operator: "and", boost: 3, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search2"}}}]}}, size: 5, from: 0};
+      return JSON.stringify(head) + '\n' + JSON.stringify(body) + '\n';
+    },
+    buildResult: function(item) {
+      return buildHTML(filterItem({
+        title :       item.title,
+        image :       (item.cover_photo) ? item.cover_photo.square.url : '',
+        description : $(item.description).text(),
+        url :         'https://events.chapman.edu/' + item.id
+      }));
+    }
+  };
+
+  // Result Type of "Post"
+  resultHandlers.post =  {
+    buildQuery: function(search_term) {
+      var head  = {index: 'posts_development'};
+      var body  = {query: {dis_max: {queries: [{match: {"text.word_start": {query: search_term, operator: "and", boost: 7, analyzer: "searchkick_word_search"}}}, {match: {"text.word_start": {query: search_term, operator: "and", boost: 5, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search"}}}, {match: {"text.word_start": {query: search_term, operator: "and", boost: 5, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search2"}}}, {match: {"_all": {query: search_term, operator: "and", boost: 3, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search"}}}, {match: {"_all": {query: search_term, operator: "and", boost: 3, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search2"}}}]}}, size: 5, from: 0};
+      return JSON.stringify(head) + '\n' + JSON.stringify(body) + '\n';
+    },
+    buildResult: function(item) {
+      return buildHTML(filterItem({
+        title :       item.text,
+        image :       (item.photos.length) ? item.photos[0].url : '',
+        description : 'By ' + item.author.display_name,
+        url :         item.external_uri
+      }));
+    }
+  };
+
+  // Maps fields from 'data' to the template vars in 'template'
+  var buildHTML = function( data ) {
+    return template.replace(/\{\{(.*?)\}\}/g, function(match, token) {
+        return data[token];
+    });
+  };
+
+  // Takes an object in, returns the same object with some stuff filtered
+  var filterItem = function(item) {
+    if (item.page_title){
+      item.page_title = item.page_title.replace('| Chapman University', '');
+    }
+
+    if (!item.image) {
+      item.image = 'https://blogs.chapman.edu/wp-content/themes/cu-wp-template-1.2/img/brand/cu_general/regular_stories/CUgeneral_default04bw.jpg';
+    }
+
+    return item;
+  }
+
 
   /******************************************/
   /***** ::: jQuery Object Instance ::: *****/
@@ -34,30 +114,14 @@
     var settings = $.extend({
     }, options );
 
-    var query_params = {
-      group_id : settings.group_id,
-    };
-
+    // References to our DOM nodes
     var $result_container     = $('<div class="search-results"></div>');
     var $result_type_selector = $(settings['selector-id']);
 
+    var types_to_search = [];
+
+
     /* ::: Instance Methods ::: */
-
-    // Maps fields from 'data' to the template vars in 'template'
-    var buildHTML = function( data ) {
-      return template.replace(/\{\{(.*?)\}\}/g, function(match, token) {
-          return data[token];
-      });
-    };
-
-    var showNoResultMessage = function( ) {
-      $self.append('<p>Sorry, there are no matching events to display. <a href="'+base_url+'">View all Chapman University events &raquo;</a></p>');
-    };
-
-    var appendMoreInfoLink = function( ) {
-      var url = base_url + '?' + $.param(query_params);
-      $self.append('<p class="more-cu-events"><a href="'+url+'">View more upcoming events &raquo;</a></p>');
-    }
 
     var getData = function() {      
       return $.ajax({
@@ -76,101 +140,61 @@
     var buildQuery = function() {
       var search_term = $self.val();
 
-      var chap_head  = {index: 'chapcrawl'};
-      var chap_body  = {query : {bool : {should : [{match: {meta_title: {query : search_term, boost : 7}}},{match: {page_title: {query : search_term, boost : 5}}},{match: {description: {query : search_term, boost : 3}}},{match: {_all: search_term}}]}}, size: 5, from: 0};
-
-      var event_head = {index: 'events_development'};
-      var event_body = {query: {dis_max: {queries: [{match: {"title.word_start": {query: search_term, operator: "and", boost: 7, analyzer: "searchkick_word_search"}}}, {match: {"title.word_start": {query: search_term, operator: "and", boost: 5, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search"}}}, {match: {"title.word_start": {query: search_term, operator: "and", boost: 5, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search2"}}}, {match: {"_all": {query: search_term, operator: "and", boost: 3, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search"}}}, {match: {"_all": {query: search_term, operator: "and", boost: 3, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search2"}}}]}}, size: 5, from: 0};
-
-      var post_head  = {index: 'posts_development'};
-      var post_body  = {query: {dis_max: {queries: [{match: {"text.word_start": {query: search_term, operator: "and", boost: 7, analyzer: "searchkick_word_search"}}}, {match: {"text.word_start": {query: search_term, operator: "and", boost: 5, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search"}}}, {match: {"text.word_start": {query: search_term, operator: "and", boost: 5, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search2"}}}, {match: {"_all": {query: search_term, operator: "and", boost: 3, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search"}}}, {match: {"_all": {query: search_term, operator: "and", boost: 3, fuzziness: 2, max_expansions: 3, analyzer: "searchkick_search2"}}}]}}, size: 5, from: 0};
-
-      return JSON.stringify(chap_head) + '\n' + JSON.stringify(chap_body) + '\n' + JSON.stringify(event_head) + '\n' + JSON.stringify(event_body) + '\n' + JSON.stringify(post_head) + '\n' + JSON.stringify(post_body) + '\n';
+      return types_to_search.map(function(type) {
+        console.log('We are searching for a: '+type);
+        console.log(resultHandlers[type].buildQuery(search_term));
+        return resultHandlers[type].buildQuery(search_term);
+      }).join('');
     };
 
     var processResults = function(results) {
 
-      var index = parseInt($result_type_selector.val());
-      var data = results.responses[index];
-      var elems = '';
+      // Put all the results into one big flattened array
+      var all_results = types_to_search.map(function(type, index) {
 
-      for (i=0; i < data.hits.hits.length; i++) {
-        var item = fastAndDirty(data.hits.hits[i]['_source'], index);
-        elems += buildHTML(item);
-      }
+        var raw_group_of_hits = results.responses[index].hits.hits;
 
-      $result_container.html(elems);
-    }
+        // Assign result type to each result within result-type group
+        var group_of_hits = raw_group_of_hits.map(function(item) {
+          item.type_of_result = type;
+          return item;
+        });
 
-    var fastAndDirty = function(item, index) {
-      switch(index){
-        case 0:
-          return buildWebResult(item);
-          break;
-        case 1:
-          return buildEventResult(item);
-          break;
-        case 2:
-          return buildPostResult(item);
-          break;
-      }
-    }
+        return group_of_hits;
 
-    // Takes in raw data, returns formatted result
-    var buildWebResult = function(item) {
-      return filterItem({
-        title :       item.page_title,
-        image :       item.page_image,
-        description : item.description,
-        url :         item.url
+      }).reduce(function(prev, cur) {
+        return prev.concat(cur);
       });
-    };
 
-    // Takes in raw data, returns formatted result
-    var buildEventResult = function(item) {
-      return filterItem({
-        title :       item.title,
-        image :       (item.cover_photo) ? item.cover_photo.square.url : '',
-        description : item.description,
-        url :         'https://events.chapman.edu/' + item.id
-      });
-    };
+      // SORTING FUNCTION GOES HERE
+      // all_results = all_results.each by score;
 
-    // Takes in raw data, returns formatted result
-    var buildPostResult = function(item) {
-      return filterItem({
-        title :       item.text,
-        image :       (item.photos.length) ? item.photos[0].url : '',
-        description : 'By ' + item.author.display_name,
-        url :         item.external_uri
-      });
-    };
+      var html = all_results.map(function(item) {
+        console.log("Trying to process a: "+item.type_of_result);
+        return resultHandlers[item.type_of_result].buildResult(item['_source']);
+      }).join('');
 
-    // Takes an object in, returns the same object with some stuff filtered
-    var filterItem = function(item) {
-      if (item.page_title){
-        item.page_title = item.page_title.replace('| Chapman University', '');
-      }
+      console.log(all_results);
 
-      if (!item.image) {
-        item.image = 'https://blogs.chapman.edu/wp-content/themes/cu-wp-template-1.2/img/brand/cu_general/regular_stories/CUgeneral_default04bw.jpg';
-      }
+      $result_container.html(html);
 
-      return item;
     }
 
     var performSearch = function() {
       getData().then(processResults).then(showResults);
-      //showResults()
     };
 
     var showResults = function() {
       $result_container.fadeIn(100);
-    }
+    };
   
     var hideResults = function() {
       $result_container.fadeOut(100);
-    }
+    };
+
+    var clearResults = function() {
+      $result_container.html('');
+    };
 
     /* ::: Main Method ::: */
     
@@ -179,7 +203,6 @@
 
     // Bind actions
     $self.on('input', debounce(performSearch, 100, false));
-    $result_type_selector.on('change', performSearch);
 
     // Close results on esc key
     $('body').on('keyup', function(e) {
@@ -192,13 +215,25 @@
     // Show results on focus
     $self.on('focus', showResults);
 
+    // Set types of results to search
+    types_to_search.push($result_type_selector.val());
+
+    // Update results to search when user changes dropdown
+    $result_type_selector.on('change', function() {
+      types_to_search = [];
+      types_to_search.push($result_type_selector.val());
+      clearResults();
+      performSearch();
+    });
+
+
     // It's the jQuery way! (Allows further command chaining)
     return this;
 
   };
 
   /************************************/
-  /***** ::: STATIC FUNCTIONS ::: *****/
+  /***** ::: LIBRARY FUNCTIONS ::: *****/
   /************************************/
   function debounce(func, wait, immediate) {
     var timeout;
